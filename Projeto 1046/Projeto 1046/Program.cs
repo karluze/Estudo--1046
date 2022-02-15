9e6ad0cd5f5b2ec930db10ef10f374a4f36b0a5c
karluze <karluzem@gmail.com>
2022-02-15 16:48:34 -0300

    Projeto 1046

---
+++ Projeto 1046/Projeto 1046/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            int total;
+            if (A < B)
+            {
+                total = B - A;
+
+            }
+            else
+            {
+                total = 24 - A + B;
+            }
+            Console.WriteLine("O JOGO DUROU " + total + " HORA(S)");
+
+            //Console.ReadLine();
+        }
+    }
+
+}
